a081438e0a0a4afab871a8f33fb891853c8e8786
Marcin Papierzyński <marcin@marcin.com>
2018-07-22 20:56:22 +0200

    midi quantization

--- style/midi.py
+++ style/midi.py
@@ -1,9 +1,11 @@
-import copy
+from copy import copy
 import os
 import re
 
 from collections import defaultdict
+from fractions import Fraction
 from py_utils import flatten
+from py_utils.math import round_number
 
 import mido
 from mido import Message, MetaMessage, MidiFile, MidiTrack
@@ -66,15 +68,20 @@ def create_midi(info, *channels):
     track.append(MetaMessage('set_tempo', tempo=info['tempo']))
 
     for channel in channels:
-        track.append(Message('program_change',
-                             channel=channel['index'], program=channel['program']))
+        if channel['index'] != 9:
+            track.append(Message('program_change',
+                                 channel=channel['index'], program=channel['program']))
+
+        if 'volume' in channel:
+            track.append(Message('control_change',
+                                 channel=channel['index'], control=7, value=channel['volume']))
 
     msgs = flatten([channel['messages'] for channel in channels])
     msgs = sorted(msgs, key=lambda x: x.time)
 
     time = 0
     for msg in msgs:
-        msg = copy.copy(msg)
+        msg = copy(msg)
         time, msg.time = msg.time, msg.time - time
         track.append(msg)
     track.append(MetaMessage('end_of_track', time=info['duration'] - time))
@@ -87,7 +94,7 @@ def merge_tracks(tracks):
     for track in tracks:
         time = 0
         for msg in track:
-            msg = copy.copy(msg)
+            msg = copy(msg)
             time += msg.time
             msg.time = time
             msgs.append(msg)
@@ -118,7 +125,7 @@ def split_channels(mid):
     tempo2time = defaultdict(int)
 
     for msg in merge_tracks(mid.tracks):
-        msg = copy.copy(msg)
+        msg = copy(msg)
         if msg.type in messages_to_ignore:
             continue
 
@@ -176,3 +183,78 @@ def split_channels(mid):
         channel['instrument_name'] = get_instrument_name(channel['program'], channel['index'])
 
     return channels, info
+
+
+def quantize_channel(info, channel, beat_divisors=[8, 3]):
+    def ticks2loc(ticks):
+        bar, ticks = divmod(ticks, info['ticks_per_bar'])
+        beat, ticks = divmod(ticks, info['ticks_per_beat'])
+
+        return bar, beat, ticks
+
+    divisor2ticks = {divisor: info['ticks_per_beat'] / divisor for divisor in beat_divisors}
+
+    def note_quantizations(time, duration=None):
+        # todo: use numpy
+        for divisor, ticks in divisor2ticks.items():
+            qtime, time_error = round_number(time, ticks)
+            total_error = abs(time_error)
+            yield (qtime, divisor), total_error
+
+    notes = []
+    pitch2note = {}
+    for msg in channel['messages']:
+        if msg.type in ['note_on', 'note_off']:
+            pitch = msg.note
+            if pitch in pitch2note:
+                note = pitch2note[pitch]
+                note['end_time'] = msg.time
+                note['duration'] = note['end_time'] - note['time']
+                del pitch2note[pitch]
+            if msg.type == 'note_on':
+                note = dict(
+                    pitch=pitch,
+                    velocity=msg.velocity,
+                    time=msg.time,
+                    time_sec=mido.tick2second(msg.time, info['ticks_per_beat'], info['tempo'])
+                )
+                notes.append(note)
+                pitch2note[pitch] = note
+
+    for note in notes:
+        time = note['time']
+        duration = note.get('duration')
+        qtime, divisor = min(note_quantizations(time, duration), key=lambda x: x[1])[0]
+        note['qtime'] = int(qtime)
+        note['qduration'] = note['end_time'] - note['qtime']
+
+        bar, beat, ticks = ticks2loc(note['qtime'])
+        note['bar'] = int(bar)
+        note['beat'] = int(beat)
+        quants = int(ticks // divisor2ticks[divisor])
+        note['beat_fraction'] = Fraction(quants, divisor)
+
+    return notes
+
+
+def dequantize_channel(info, channel_info, quantized_channel):
+    def loc2ticks(bar, beat, beat_fraction):
+        return (
+            bar * info['ticks_per_bar'] +
+            beat * info['ticks_per_beat'] +
+            int(beat_fraction * info['ticks_per_beat'])
+        )
+
+    messages = []
+    channel_idx = channel_info['index']
+    for note in quantized_channel:
+        time = loc2ticks(note['bar'], note['beat'], note['beat_fraction'])
+        note_on = Message('note_on', channel=channel_idx,
+                          note=note['pitch'], velocity=note['velocity'], time=time)
+        note_off = Message('note_off', channel=channel_idx,
+                           note=note['pitch'], time=time+note['qduration'])
+        messages += [note_on, note_off]
+
+    channel = copy(channel_info)
+    channel['messages'] = sorted(messages, key=lambda x: x.time)
+    return channel
