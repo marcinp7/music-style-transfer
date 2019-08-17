f46906d367a194adeb72779fb744e8e7a2ead0ae
Marcin Papierzyński <marcin@marcin.com>
2019-08-17 16:12:05 +0200

    New models

--- style/model.py
+++ style/model.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from py_utils.pytorch import total_size, remove_dims, tensor_view, Distributed, squash_dims, LSTM
+from py_utils.pytorch import remove_dims, tensor_view, Distributed, squash_dims, LSTM
 
 # from style.midi import get_input
 
@@ -15,7 +15,7 @@ def flatten_channel(x):
 
 
 class ChannelEncoder(nn.Module):
-    def __init__(self, n_channels=100, beats_lstm_size=100, bars_lstm_size=100):
+    def __init__(self, n_channels=50, beat_size=16, bar_size=32):
         super().__init__()
         self.beat_conv = nn.Conv1d(
             in_channels=50,
@@ -27,14 +27,14 @@ class ChannelEncoder(nn.Module):
         self.beat_conv = Distributed(self.beat_conv, depth=2)
         self.beats_lstm = LSTM(
             input_size=8*n_channels,
-            hidden_size=beats_lstm_size,
+            hidden_size=beat_size,
             num_layers=1,
             batch_first=True,
         )
         self.beats_lstm = Distributed(self.beats_lstm, depth=1)
         self.bars_lstm = LSTM(
-            input_size=4*beats_lstm_size,
-            hidden_size=bars_lstm_size,
+            input_size=4*beat_size,
+            hidden_size=bar_size,
             num_layers=1,
             bidirectional=True,
             batch_first=True,
@@ -42,18 +42,19 @@ class ChannelEncoder(nn.Module):
 
     def forward(self, x):
         # input format: (batch, bar, beat, beat_fraction, note_features, note)
-        x = squash_dims(x, 3, 5)  # combine beat fractions and note features
+        if len(x.shape) == 6:
+            x = squash_dims(x, 3, 5)  # combine beat fractions and note features
         x = self.beat_conv(x)
         x = F.relu(x)
         x = squash_dims(x, -2)
-        x = self.beats_lstm(x)[0]
-        x = squash_dims(x.contiguous(), 2)
-        x = self.bars_lstm(x)[0]
-        return x
+        beats = self.beats_lstm(x)[0]
+        x = squash_dims(beats.contiguous(), 2)
+        bars = self.bars_lstm(x)[0]
+        return beats, bars
 
 
 class StyleEncoder(nn.Module):
-    def __init__(self, input_size=200, hidden_size=100):
+    def __init__(self, input_size=64, hidden_size=100):
         super().__init__()
         self.lstm = LSTM(
             input_size=input_size,
@@ -62,16 +63,15 @@ class StyleEncoder(nn.Module):
             batch_first=True,
         )
 
-    def forward(self, x):
-        x = self.lstm(x)[0]
+    def forward(self, bars):
+        x = self.lstm(bars)[0]
         x = x[:, -1]
         return x
 
 
 class MelodyEncoder(nn.Module):
-    def __init__(self, channel_encoder=None):
+    def __init__(self):
         super().__init__()
-        self.channel_encoder = channel_encoder
         self.beat_conv = nn.Conv1d(
             in_channels=50,
             out_channels=7,
@@ -80,21 +80,28 @@ class MelodyEncoder(nn.Module):
             padding=4,
         )
         self.beat_conv = Distributed(self.beat_conv, depth=2)
-        self.linear = nn.Linear(
-            in_features=200,
+        self.beats_linear = nn.Linear(
+            in_features=32,
+            out_features=7,
+        )
+        self.bars_linear = nn.Linear(
+            in_features=64,
             out_features=7,
         )
 
-    def forward(self, channel, encoded_channel=None):
-        channel = squash_dims(channel, 3, 5)  # combine beat fractions and note features
+    def forward(self, channel, beats, bars):
+        if len(channel.shape) == 6:
+            channel = squash_dims(channel, 3, 5)  # combine beat fractions and note features
         x1 = self.beat_conv(channel)
 
-        if encoded_channel is None:
-            encoded_channel = self.channel_encoder(channel)
-        x2 = self.linear(encoded_channel)
-        x2 = x2.unsqueeze(-1).unsqueeze(2)
+        x2 = self.beats_linear(beats)
+        x2 = x2.unsqueeze(-1)
 
-        x = x1 + x2
+        x3 = self.bars_linear(bars)
+        x3 = x3.unsqueeze(-1).unsqueeze(2)
+
+        x = x1 + x2 + x3
+        x = x.transpose(3, 4).contiguous() # octave must come before scale degree
         x = squash_dims(x, -2)
         x = torch.sigmoid(x)
         # x = F.hardtanh(x, 0., 1.)
@@ -106,69 +113,76 @@ class MelodyEncoder(nn.Module):
 
 
 class StyleApplier(nn.Module):
-    def __init__(self, n_channels, beats_lstm_size, bars_lstm_size):
+    def __init__(self):
         super().__init__()
-        self.beat_conv = nn.Conv1d(
+        self.channel_encoder = ChannelEncoder(n_channels=10, beat_size=4, bar_size=8)
+        out_features = 10 * 5 * 8 * 7
+        self.beats_linear = nn.Linear(
+            in_features=4,
+            out_features=out_features,
+        )
+        self.bars_linear = nn.Linear(
+            in_features=16,
+            out_features=out_features,
+        )
+        self.style_linear = nn.Linear(
+            in_features=100,
+            out_features=out_features,
+        )
+        self.beats_conv = nn.Conv1d(
             in_channels=50,
-            out_channels=n_channels,
+            out_channels=7*50,
             kernel_size=14,
             stride=7,
             padding=4,
         )
-        self.beat_conv = Distributed(self.beat_conv, depth=2)
-        self.beats_lstm = LSTM(
-            input_size=8*n_channels,
-            hidden_size=beats_lstm_size,
-            num_layers=1,
-            batch_first=True,
-        )
-        self.beats_lstm = Distributed(self.beats_lstm, depth=1)
-        self.bars_lstm = LSTM(
-            input_size=4*beats_lstm_size,
-            hidden_size=bars_lstm_size,
-            num_layers=1,
-            bidirectional=True,
-            batch_first=True,
-        )
+        self.beats_conv = Distributed(self.beats_conv, depth=2)
 
-    def duration_activation(self, x):
-        return torch.relu(x)
-
-    def velocity_activation(self, x):
+    @classmethod
+    def duration_activation(cls, x):
         x = torch.relu(x)
-        return torch.sigmoid(x)
+        return x
 
-    def apply_style(self, bar, style):
-        n_beats = bar.shape[0]
-        style = style.unsqueeze(0).expand(n_beats, -1, -1)
-        concat_bar = torch.cat([bar, style], dim=2)
-        output, _ = self.lstm(concat_bar)
-        output = self.fc(output)
-        return output
+    @classmethod
+    def velocity_activation(cls, x):
+        x = torch.relu(x)
+        return x
+
+    @classmethod
+    def accidental_activation(cls, x):
+        x = torch.sigmoid(x)
+        return x
 
     def forward(self, melody, style):
-        x = self.beat_conv(melody)
-        x = F.relu(x)
+        beats, bars = self.channel_encoder(melody)
+
+        x1 = self.beats_linear(beats)
+        # print(x1.shape)
+
+        x2 = self.bars_linear(bars)
+        x2 = x2.unsqueeze(2)
+        # print(x2.shape)
+
+        x3 = self.style_linear(style)
+        x3 = x3.unsqueeze(1).unsqueeze(1)
+        # print(x3.shape)
+
+        x4 = self.beats_conv(melody)
+        x4 = x4.view(*x4.shape[:3], 10, 5, 7, 8)
+        x4 = x4.transpose(-1, -2) # octave must come before scale degree
+        # print(x4.shape)
+
+        x = x1 + x2 + x3
+        x = x.view(*x.shape[:3], 10, 5, 8, 7)
+        x += x4
         x = squash_dims(x, -2)
-        x = self.beats_lstm(x)[0]
-        x = squash_dims(x.contiguous(), 2)
-        x = self.bars_lstm(x)[0]
+        # print(x.shape)
 
+        x[:, :, :, :, 0, :] = self.duration_activation(x[:, :, :, :, 0, :])
+        x[:, :, :, :, 1, :] = self.velocity_activation(x[:, :, :, :, 1, :])
+        x[:, :, :, :, 2:, :] = self.accidental_activation(x[:, :, :, :, 2:, :])
 
-        melody = flatten_channel(melody)
-        bars = [self.apply_style(bar, style) for bar in melody]
-        output = torch.stack(bars)
-        output = tensor_view(output, keep_dims=2, *self.output_shape)
-
-        durations = output[:, :, :, :, 0]
-        velocities = output[:, :, :, :, 1]
-
-        durations = self.duration_activation(durations)
-        velocities = self.velocity_activation(velocities)
-
-        output = torch.stack([durations, velocities], dim=-1)
-
-        return output
+        return x
 
 
 class StyleTransferModel(nn.Module):
